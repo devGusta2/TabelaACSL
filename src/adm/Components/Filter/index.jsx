62b52d1b912d0614eec20b53b22e6ed49224bc12
devGusta2 <gustavo.souza2979@gmail.com>
2025-02-05 20:00:49 -0300

    Novo visual para o filtro

--- src/adm/Components/Filter/index.jsx
+++ src/adm/Components/Filter/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './index.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faPlus} from '@fortawesome/free-solid-svg-icons';
+import { faCalendar, faCheck, faPlus } from '@fortawesome/free-solid-svg-icons';
 const FilterModal = ({ onClose, onApply }) => {
     const [localFilters, setLocalFilters] = useState([{ reference_year: 2025, reference_month: 1 }]);
 
@@ -18,27 +18,44 @@ const FilterModal = ({ onClose, onApply }) => {
     return (
         <div className="modal">
             <div className="modal_content">
-                <h2>Filtrar Anúncios</h2>
+                <h2>Filtro de anúncios</h2>
                 {localFilters.map((filter, index) => (
                     <div key={index} className="filter_item">
-                        <input
-                            type="number"
-                            placeholder="Ano"
-                            value={filter.reference_year}
-                            onChange={(e) => updateFilter(index, 'reference_year', e.target.value)}
-                        />
-                        <input
-                            type="number"
-                            placeholder="Mês"
-                            value={filter.reference_month}
-                            onChange={(e) => updateFilter(index, 'reference_month', e.target.value)}
-                        />
+                        <div className='icon-inpt'>
+                            <div className="icon-label">
+                                <FontAwesomeIcon icon={faCalendar} className='icon-filter' size='2x' color='#EF44A1' />
+                                <label>Ano:</label>
+                            </div>
+                            <input
+                                type="number"
+                                placeholder="Ano"
+                                min='1950'
+                                value={filter.reference_year}
+                                onChange={(e) => updateFilter(index, 'reference_year', e.target.value)}
+                            />
+                        </div>
+                        <div className='icon-inpt'>
+
+                            <div className="icon-label">
+                                <FontAwesomeIcon icon={faCalendar} className='icon-filter' size='2x' color='#EF44A1' />
+                                <label>Mês:</label>
+                            </div>
+                            <input
+                                type="number"
+                                placeholder="Mês"
+                                min='1'
+                                max='12'
+                                value={filter.reference_month}
+                                onChange={(e) => updateFilter(index, 'reference_month', e.target.value)}
+                            />
+                        </div>
+
                     </div>
                 ))}
                 <div className='btn-filter-box'>
-                    <button onClick={addFilter}><FontAwesomeIcon icon={faPlus} size='2x'/>Adicionar Filtro</button>
-                    <button onClick={() => onApply(localFilters)}><FontAwesomeIcon icon={faCheck} size='2x'/>Aplicar Filtros</button>
-                 
+                    <button onClick={addFilter}><FontAwesomeIcon icon={faPlus} size='2x' />Adicionar Filtro</button>
+                    <button onClick={() => onApply(localFilters)}><FontAwesomeIcon icon={faCheck} size='2x' />Aplicar Filtros</button>
+
                 </div>
             </div>
         </div>
