44bf28d38fc6a6ea190d5c42311e1b3a33d143ae
Mel-Rocha <rochamel73@gmail.com>
2025-02-23 14:22:44 -0300

    Colocando um carro rosa na home principal para manter a paleta de cores do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import car from '../src/assets/carrao.png';
+import car from '../src/assets/pink-car.png';
 import logo from '../src/assets/logo4.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
