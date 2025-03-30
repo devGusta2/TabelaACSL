438bc82eb5f95814690ecfbb265e64233be7ac8d
Mel-Rocha <rochamel@gmail.com>
2025-03-30 18:14:26 -0300

    está funcional mas o layout está bem feio

--- src/Pages/Insights/Insights.jsx
+++ src/Pages/Insights/Insights.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Insights.css';
 import Menu from '../Components/Menu';
-import { faCalendar, faArrowUp, faArrowDown, faChartBar, faCheck, faChartColumn } from '@fortawesome/free-solid-svg-icons';
+import { faCalendar, faChartBar } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const host_django = import.meta.env.VITE_API_URL_DJANGO;
 
@@ -11,7 +11,7 @@ const Insights = () => {
     const [year, setYear] = useState(new Date().getFullYear());
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-    const [analyzeData, setAnalyzeData] = useState(null);
+    const [insightsData, setInsightsData] = useState(null);
 
     const handleAnalyze = async () => {
         setLoading(true);
@@ -26,7 +26,7 @@ const Insights = () => {
 
         try {
             const response = await axios.get(
-                `${host_django}/crawler/calcs/analyze/machine/${year}/${month}/`,
+                `${host_django}/crawler/calcs/insights/machine/${year}/${month}/`,
                 {
                     headers: {
                         'User-Agent': 'insomnia/10.1.1',
@@ -35,9 +35,9 @@ const Insights = () => {
                     },
                 }
             );
-            setAnalyzeData(response.data.content);
+            setInsightsData(response.data.content);
         } catch (error) {
-            console.error('Erro ao obter análise:', error);
+            console.error('Erro ao obter insights:', error);
             setError(error.response ? error.response.data : 'Erro desconhecido');
         } finally {
             setLoading(false);
@@ -57,17 +57,9 @@ const Insights = () => {
                     <span>Informações inteligentes sobre Precificação de Veículos</span>
                 </div>
 
-
-
-
                 {loading && <p className="loading-message">Carregando...</p>}
                 {error && <p className="error-message">Erro: {error}</p>}
-                {analyzeData && (
-
-
-
-
-
+                {insightsData && (
                     <div id="cards_info_box">
                         <div className="row_cards">
                             <div id="data_box">
@@ -76,7 +68,8 @@ const Insights = () => {
                                         <FontAwesomeIcon size='3x' icon={faCalendar} />
                                         <label>Mês</label>
                                     </div>
-                                    <input placeholder='Insira um mês'
+                                    <input
+                                        placeholder='Insira um mês'
                                         type="number"
                                         min="1"
                                         max="12"
@@ -97,99 +90,23 @@ const Insights = () => {
                                         value={year}
                                         onChange={(e) => setYear(Number(e.target.value))}
                                         required
-                                        placeholder='Insira ano modelo' />
-                                </div>
-                            </div>
-                            <div id="col_cards">
-                                <div className="dp_val_card">
-                                    <div className="icon_box_val">
-                                        <FontAwesomeIcon className='icon_up' icon={faArrowUp} />
-                                    </div>
-                                    <div className="icon_box_content">
-                                        <span className='title_val'>Valorização Máxima</span>
-                                        {/* <span className='content_val'>O modelo MC00100 teve a maior valorização acumulada, com um ganho de R$1410000.00 ao longo dos anos modelos analisados.</span> */}
-                                        <span className='content_val'>{analyzeData.analysis["Valorização Máxima"]}</span>         
-                                    </div>
-                                </div>
-                                <div className="dp_val_card">
-                                    <div className="icon_box_val">
-                                        <FontAwesomeIcon className='icon_up' icon={faArrowDown} />
-                                    </div>
-                                    <div className="icon_box_content">
-                                        <span className='title_val'>Valorização Máxima</span>
-                                        {/* <span className='content_val'>O modelo MC00100 teve a maior valorização acumulada, com um ganho de R$1410000.00 ao longo dos anos modelos analisados.</span> */}
-                                        <span className='content_val'>{analyzeData.analysis["Depreciação Máxima"]}</span>
-                                    </div>
+                                        placeholder='Insira ano modelo'
+                                    />
                                 </div>
                             </div>
                         </div>
                         <div className="row_cards">
-                            <div className="dp_val_card1">
-                                <div className="icon_box_val1">
-                                    <FontAwesomeIcon className='icon_up1' icon={faCheck} />
-                                </div>
-                                <div className="icon_box_content">
-                                    <span className='title_val'>Modelo Mais Consistente</span>
-                                    {/* <span className='content_val'>O modelo MC00100 teve a maior valorização acumulada, com um ganho de R$1410000.00 ao longo dos anos modelos analisados.</span> */}
-                                    <span className='content_val'>{analyzeData.analysis["Modelo Mais Consistente"]}</span>
-                                </div>
-                                <div className="icon_box_val1">
-                                    <FontAwesomeIcon className='icon_up1' icon={faChartColumn} />
-                                </div>
-                                <div className="icon_box_content">
-                                    <span className='title_val'>Modelo Mais Variado</span>
-                                    {/* <span className='content_val'>O modelo MC00100 teve a maior valorização acumulada, com um ganho de R$1410000.00 ao longo dos anos modelos analisados.</span> */}
-                                    <span className='content_val'>{analyzeData.analysis["Modelo Mais Variado"]}</span>
-                                </div>
+                            <div className="insights">
+                                <h2>Insights para {insightsData.year_reference}/{insightsData.month_reference}</h2>
+                                <ul>
+                                    {insightsData.insights.map((insight, index) => (
+                                        <li key={index}>{insight}</li>
+                                    ))}
+                                </ul>
                             </div>
-
                         </div>
-
                     </div>
                 )}
-
-                {/* <div className="insights-wrapper">
-                    <div className="insights-header">
-                        <h1>Insights sobre Precificação de Veículos</h1>
-                        <div className="date-selection">
-                            <label>
-                                Mês:
-                                <input
-                                    type="number"
-                                    min="1"
-                                    max="12"
-                                    value={month}
-                                    onChange={(e) => setMonth(Number(e.target.value))}
-                                    required
-                                />
-                            </label>
-                            <label>
-                                Ano:
-                                <input
-                                    type="number"
-                                    min="2024"
-                                    max={new Date().getFullYear()}
-                                    value={year}
-                                    onChange={(e) => setYear(Number(e.target.value))}
-                                    required
-                                />
-                            </label>
-                        </div>
-                    </div>
-                    <div className="insights-container">
-                        {loading && <p className="loading-message">Carregando...</p>}
-                        {error && <p className="error-message">Erro: {error}</p>}
-                        {analyzeData && (
-                            <div className="analyze-data">
-                                <h2>Análise para {analyzeData.year_reference}/{analyzeData.month_reference}</h2>
-                                <p><strong>Valorização Máxima:</strong> {analyzeData.analysis["Valorização Máxima"]}</p>
-                                <p><strong>Depreciação Máxima:</strong> {analyzeData.analysis["Depreciação Máxima"]}</p>
-                                <p><strong>Modelo Mais Consistente:</strong> {analyzeData.analysis["Modelo Mais Consistente"]}</p>
-                                <p><strong>Modelo Mais Variado:</strong> {analyzeData.analysis["Modelo Mais Variado"]}</p>
-                            </div>
-               
-                    </div>
-                </div> */}
             </div>
         </div>
     );
