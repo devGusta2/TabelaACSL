0b4bf868593b7c24e7e189660d14836c635258cb
devGusta2 <gustavo.souza2979@gmail.com>
2025-04-02 20:03:08 -0300

    Mudanças nas páginas dedicadas à descrição, cores de acordo com a paleta padrão, e layout mais clean

--- src/Pages/Description/Description-1/index.jsx
+++ src/Pages/Description/Description-1/index.jsx
@@ -1,58 +1,74 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
 import './index.css'
+import { Link } from "react-router-dom";
 export default function Description1(){
     return (
         <div className="container">
-          <h1 className="title">DriveIntel: Inteligência de Mercado Automotivo</h1>
+          <Link style={{
+            display:'flex',
+            alignItems:'Center',
+            gap:'1em',
+            textDecoration:'none',
+            color:'#EF44A1'
+
+          }}to='../'><FontAwesomeIcon icon={faArrowLeft} size="3x"/>Voltar</Link>
+          <h1 className="title" style={{color:'#EF44A1'}}>DriveIntel: Inteligência de Mercado Automotivo</h1>
     
           <div className="relevant-section">
-            <h2 className="section-title">Para quem o DriveIntel é relevante:</h2>
+            <h2 className="section-title" >Para quem o DriveIntel é relevante:</h2>
+           
             <ul className="list">
-              <li>
+              <li style={{backgroundColor:'#FA8FB1'}}>
                 <i className="fas fa-store-alt"></i>
                 <span>Lojistas e concessionárias</span>
               </li>
-              <li>
+              <li style={{backgroundColor:'#FA8FB1'}}>
                 <i className="fas fa-car"></i>
                 <span>Gestores de frotas</span>
               </li>
-              <li>
+              <li style={{backgroundColor:'#FA8FB1'}}>
                 <i className="fas fa-truck"></i>
                 <span>Empresas de aluguel de veículos</span>
               </li>
-              <li>
+              <li style={{backgroundColor:'#FA8FB1'}}>
                 <i className="fas fa-chart-line"></i>
                 <span>Investidores no mercado automotivo</span>
-              </li>
-              <li>
+              </li >
+              <li style={{backgroundColor:'#FA8FB1'}}>
                 <i className="fas fa-credit-card"></i>
                 <span>Instituições financeiras</span>
               </li>
-              <li>
+              <li style={{backgroundColor:'#FA8FB1'}}>
                 <i className="fas fa-gavel"></i>
                 <span>Leiloeiros</span>
               </li>
             </ul>
+            <div className="desc-publico-alvo-page">
+            ✔ Lojistas e concessionárias – Para definir preços competitivos e antecipar variações no mercado.✔ Gestores de frotas – Para calcular o melhor momento de compra e venda de veículos.✔ Empresas de aluguel de veículos – Para planejar renovação de frota com base em previsões de preço.✔ Investidores no mercado automotivo – Para identificar oportunidades de compra e revenda.✔ Instituições financeiras – Para precificação de garantias em financiamentos e seguros automotivos.✔ Leiloeiros – Para estimar valores justos de venda e prever tendências do mercado.
+            </div>
           </div>
     
           <div className="not-relevant-section">
             <h2 className="section-title">Para quem o DriveIntel não é relevante:</h2>
             <ul className="list">
-              <li>
+              <li style={{backgroundColor:'#FA8FB1'}}>
                 <i className="fas fa-times-circle"></i>
                 <span>Consumidores finais</span>
               </li>
-              <li>
+              <li style={{backgroundColor:'#FA8FB1'}}>
                 <i className="fas fa-times-circle"></i>
                 <span>Pequenos revendedores sem estratégia de dados</span>
               </li>
-              <li>
+              <li style={{backgroundColor:'#FA8FB1'}}>
                 <i className="fas fa-tools"></i>
                 <span>Mecânicos e oficinas</span>
               </li>
             </ul>
+            <div className="desc-publico-alvo-page">
+            ❌ Consumidores finais que compram apenas um carro para uso próprio – O produto foca em inteligência de mercado e previsões para grandes volumes.❌ Pequenos revendedores sem estratégia baseada em dados – Se o modelo de negócio for apenas baseado em feeling e experiência pessoal, sem interesse em análise de mercado, o DriveIntel pode ser subutilizado.❌ Mecânicos e oficinas – O foco do produto é precificação de veículos, não manutenção ou reparo.
+            </div>
           </div>
         </div>
       );
