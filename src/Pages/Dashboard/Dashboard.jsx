e8f7a41ac7f82f485f0555d9c523d1f9f334118c
devGusta2 <gustavo.souza2979@gmail.com>
2025-04-02 21:05:48 -0300

    Icones arrumados

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,14 @@
+
 import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
+import Tooltip from '../Components/Tooltip/Tooltip';
 import {
     ComposedChart,
     Bar,
     Scatter,
     XAxis,
     YAxis,
-    Tooltip,
+
     CartesianGrid,
     ResponsiveContainer,
     ScatterChart
@@ -127,105 +129,99 @@ const Dashboard = () => {
                                 aria-label="Selecione o ano"
                             />
                         </label>
-                   </div>
-    {loadingCount > 0 && <p className="loading-message">Carregando...</p>}
-    {error && <p className="error-message">Erro: {error}</p>}
-    {(kpiData || {}) && (
-        <div className="kpi-data">
-            <div className="kpi-header">
-                <h2 className="text-xl">Visão Geral do Mercado</h2>
-
-                {/* Ícone de informação com tooltip */}
-                <div className="tooltip-container">
-                    <div className="icon-info">i</div>
-                    <div className="tooltip">
-                        Tais indicadores representam um panorama abrangente do mercado automotivo
-                        para o período selecionado. O recorte considera <strong>todos</strong>  os anúncios
-                        disponíveis no mês e ano de referência, permitindo uma análise completa da oferta de veículos.
                     </div>
+                    {loadingCount > 0 && <p className="loading-message">Carregando...</p>}
+                    {error && <p className="error-message">Erro: {error}</p>}
+                    {(kpiData || {}) && (
+                        <div className="kpi-data">
+                            <div className="kpi-header">
+                                <h2 className="text-xl">Visão Geral do Mercado</h2>
+
+                                {/* Ícone de informação com tooltip */}
+                                <Tooltip text="
+                             Tais indicadores representam um panorama abrangente do mercado automotivo
+                                        para o período selecionado. O recorte considera <strong>todos</strong>  os anúncios
+                                        disponíveis no mês e ano de referência, permitindo uma análise completa da oferta de veículos.">
+                                    <div ></div>
+                                </Tooltip>
+
+                            </div>
+
+                            <p><strong>Total de anúncios:</strong> {kpiData?.total_ads ?? 0}</p>
+                            <p><strong>Ano modelo médio:</strong> {kpiData?.total_average_year_model ?? 0}</p>
+                            <p><strong>Preço médio:</strong> R$ {(kpiData?.total_average_price ?? 0).toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</p>
+                        </div>
+                    )}
                 </div>
-            </div>
-
-            <p><strong>Total de anúncios:</strong> {kpiData?.total_ads ?? 0}</p>
-            <p><strong>Ano modelo médio:</strong> {kpiData?.total_average_year_model ?? 0}</p>
-            <p><strong>Preço médio:</strong> R$ {(kpiData?.total_average_price ?? 0).toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</p>
-        </div>
-    )}
-</div>
 
 
-             <div className="dashboard-container">
-                <div id="title_chart_box">
-                    <h2 className="text-lg">Dispersão de Preços</h2>
-                    <div className="tooltip-container">
-                        <div className="icon-info">i</div>
- <div className="tooltip">
-    Tais dados demonstram a distribuição dos preços mais ofertados, a análise se embasa no recorte dos <strong>10 preços mais frequentes</strong> do mercado para as determinadas datas de referência, evidenciando as faixas mais comuns de preço, revelando assim tendências predominantes e os clusters de mercado na área de precificação.
-</div>
+                <div className="dashboard-container">
+                    <div id="title_chart_box">
+                        <h2 className="text-lg">Dispersão de Preços</h2>
+                        <Tooltip text="
+                             Tais dados demonstram a distribuição dos preços mais ofertados, a análise se embasa no recorte dos <strong>10 preços mais frequentes</strong> do mercado para as determinadas datas de referência, evidenciando as faixas mais comuns de preço, revelando assim tendências predominantes e os clusters de mercado na área de precificação.">
+                            <div ></div>
+                        </Tooltip>
+
+
+                        <h2 className="text-lg">Dispersão de Ano Modelo</h2>
+                        <Tooltip text="
+                              Os seguintes dados mostram os anos-modelo mais ofertados, com o recorte dos <strong>10 anos-modelo mais frequentes</strong>. A análise revela as faixas de anos com maior presença no setor automotivo, facilitando a identificação de períodos em que o mercado está mais aquecido.">
+                            <div ></div>
+                        </Tooltip>
+
                     </div>
 
-                    <h2 className="text-lg">Dispersão de Ano Modelo</h2>
-                    <div className="tooltip-container">
-                        <div className="icon-info">i</div>
-                        <div className="tooltip">
-                             Os seguintes dados mostram os anos-modelo mais ofertados, com o recorte dos <strong>10 anos-modelo mais frequentes</strong>. A análise revela as faixas de anos com maior presença no setor automotivo, facilitando a identificação de períodos em que o mercado está mais aquecido.
+                    <div className="charts">
+                        <div className="row-chart">
+                            <ResponsiveContainer width="50%" height="100%">
+                                <ScatterChart>
+                                    <CartesianGrid strokeDasharray="3 3" />
+                                    <XAxis dataKey="x" name="Preço" label={{ value: 'Preço', position: 'insideBottom', offset: -5 }} />
+                                    <YAxis dataKey="y" name="Quantidade de Anúncios" label={{ value: 'Quant. Anúncios', angle: -90, position: 'insideLeft', dy: 80 }} />
 
+                                    <Scatter name="Preço" data={priceScatterData} fill="#ec4899" />
+                                </ScatterChart>
+                            </ResponsiveContainer>
+
+                            <ResponsiveContainer width="50%" height="100%">
+                                <ScatterChart>
+                                    <CartesianGrid strokeDasharray="3 3" />
+                                    <XAxis dataKey="x" name="Ano Modelo" label={{ value: 'Ano Modelo', position: 'insideBottom', offset: -5 }} />
+                                    <YAxis dataKey="y" name="Quantidade de Anúncios" label={{ value: 'Quant. Anúncios', angle: -90, position: 'insideLeft', dy: 80 }} />
+
+                                    <Scatter name="Ano Modelo" data={scatterData} fill="#ec4899" />
+                                </ScatterChart>
+                            </ResponsiveContainer>
+                        </div>
+
+                        <div id="title_chart_box" >
+                            <h2 className="text-lg mt-6">Correlação de Preço Médio e Quantidade de Anúncios por Estado</h2>
+                            <Tooltip text="
+                             A análise considera os <strong>10 estados com maior número de anúncios</strong>, permitindo entender a relação entre a oferta de veículos e os preços médios em regiões com maior representatividade no mercado.">
+                                <div ></div>
+                            </Tooltip>
+
+                        </div>
+                        <div className="row-chart">
+                            <ResponsiveContainer width="90%" height="100%">
+                                <ComposedChart data={geoChartData}>
+                                    <CartesianGrid strokeDasharray="3 3" />
+                                    <XAxis dataKey="name" label={{ value: 'Estado', position: 'insideBottom', offset: -5 }} />
+                                    <YAxis yAxisId="left" orientation="left" label={{ value: 'Quant. Anúncios', angle: -90, position: 'insideLeft' }} />
+                                    <YAxis yAxisId="right" orientation="right" label={{ value: 'Preço Médio', angle: -90, position: 'insideRight' }} />
+
+                                    <Bar yAxisId="left" dataKey="total_ads" fill="#ffcfe9" name="Quant. Anúncios" />
+                                    <Scatter yAxisId="right" dataKey="average_price" fill="#ec4899" name="Preço Médio" />
+                                </ComposedChart>
+                            </ResponsiveContainer>
                         </div>
                     </div>
                 </div>
-
-                <div className="charts">
-                    <div className="row-chart">
-                        <ResponsiveContainer width="50%" height="100%">
-                            <ScatterChart>
-                                <CartesianGrid strokeDasharray="3 3" />
-                                <XAxis dataKey="x" name="Preço" label={{ value: 'Preço', position: 'insideBottom', offset: -5 }} />
-                                <YAxis dataKey="y" name="Quantidade de Anúncios" label={{ value: 'Quant. Anúncios', angle: -90, position: 'insideLeft', dy: 80 }} />
-                                <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-                                <Scatter name="Preço" data={priceScatterData} fill="#ec4899" />
-                            </ScatterChart>
-                        </ResponsiveContainer>
-
-                        <ResponsiveContainer width="50%" height="100%">
-                            <ScatterChart>
-                                <CartesianGrid strokeDasharray="3 3" />
-                                <XAxis dataKey="x" name="Ano Modelo" label={{ value: 'Ano Modelo', position: 'insideBottom', offset: -5 }} />
-                                <YAxis dataKey="y" name="Quantidade de Anúncios" label={{ value: 'Quant. Anúncios', angle: -90, position: 'insideLeft', dy: 80 }} />
-                                <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-                                <Scatter name="Ano Modelo" data={scatterData} fill="#ec4899" />
-                            </ScatterChart>
-                        </ResponsiveContainer>
-                    </div>
-
-                  <div id="title_chart_box">
-    <h2 className="text-lg mt-6">Correlação de Preço Médio e Quantidade de Anúncios por Estado</h2>
-    <div className="tooltip-container">
-        <div className="icon-info">i</div>
-        <div className="tooltip">
-                       A análise considera os <strong>10 estados com maior número de anúncios</strong>, permitindo entender a relação entre a oferta de veículos e os preços médios em regiões com maior representatividade no mercado.
-
-        </div>
-    </div>
-</div>
-                    <div className="row-chart">
-                        <ResponsiveContainer width="90%" height="100%">
-                            <ComposedChart data={geoChartData}>
-                                <CartesianGrid strokeDasharray="3 3" />
-                                <XAxis dataKey="name" label={{ value: 'Estado', position: 'insideBottom', offset: -5 }} />
-                                <YAxis yAxisId="left" orientation="left" label={{ value: 'Quant. Anúncios', angle: -90, position: 'insideLeft' }} />
-                                <YAxis yAxisId="right" orientation="right" label={{ value: 'Preço Médio', angle: -90, position: 'insideRight' }} />
-                                <Tooltip />
-                                <Bar yAxisId="left" dataKey="total_ads" fill="#ffcfe9" name="Quant. Anúncios" />
-                                <Scatter yAxisId="right" dataKey="average_price" fill="#ec4899" name="Preço Médio" />
-                            </ComposedChart>
-                        </ResponsiveContainer>
-                    </div>
-                </div>
             </div>
         </div>
-    </div>
 
-);
+    );
 };
 
 export default Dashboard;
